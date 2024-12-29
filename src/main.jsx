6f8bb36d8c63932e2b8fbc4661c4f378a521cdae
anus26 <anusraza268gmail.com>
2024-12-29 03:41:29 -0800

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Layout from './Components/Layout.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Notfound from './pages/Notfound.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/login', element: <Login /> },
+      { path: '/register', element: <Register /> },
+    ],
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
