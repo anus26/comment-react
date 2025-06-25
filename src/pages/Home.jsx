f88efd426389aa8a74400616a3a63f2d9720c6ac
anus26 <anusraza268gmail.com>
2025-06-24 18:32:21 -0700

    images set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -180,7 +180,7 @@ const Home = () => {
   
     <div className="container mx-auto p-4 ">
       {error && <p className="text-red-500">Error: {error}</p>}
-      <img src="https://yourwebsite.com/images/image-png.jpg" alt="Blog" width={1500} />
+      <img src='./images/image-png.jpg'alt="Blog" width={1500} />
 
       {postData.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
